cd9f78185d767fab318c681293e74707d7a3408d
Coreallll <100600313+Coreallll@users.noreply.github.com>
2023-02-16 13:27:54 +0300

    prod commit

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+//Валидация формы
+let form = document.querySelector('form'),
+    formInputs = document.querySelectorAll('input'),
+    name = document.querySelector('.input-name'),
+    email = document.querySelector('.input-email'),
+    phone = document.querySelector('.input-phone'),
+    errName = document.querySelector('.error-name'),
+    errPhone = document.querySelector('.error-phone'),
+    errEmail = document.querySelector('.error-email');
+
+function validationEmail(item) {
+  let check = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/;
+  return check.test(String(item).toLowerCase());
+}
+
+function validationName(item) {
+  let alphabet = /^[а-яёА-ЯЁ\s]+$/;
+  return alphabet.test(item);
+}
+
+function validatePhone(item) {
+  let re = /^[\d\s]*$/;
+  return re.test(String(item));
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // formInputs.forEach((input) => {
+  //   if (input.value.trim() === '') {
+  //     input.classList.add('input--invalid');
+  //     errName.textContent = 'Поле необходимо заполнить'
+  //   } else {
+  //     input.classList.remove('input--invalid');
+  //   }
+  // })
+
+
+  if (name.value.trim() === '') {
+    errName.textContent = 'Поле необходимо заполнить'
+    name.classList.add('input--invalid');
+  } else if(name.value.length > 0 && name.value.length <= 3){
+    errName.textContent = 'Имя слишком короткое';
+  } else {
+    errName.textContent = '';
+    name.classList.remove('input--invalid');
+  }
+
+  if (phone.value.trim() === '') {
+    errPhone.textContent = 'Поле необходимо заполнить'
+    phone.classList.add('input--invalid');
+  } else if(!validatePhone(phone.value)) {
+    phone.classList.add('input--invalid');
+    errPhone.textContent = 'Поле не может содержать буквы'
+  } else if(phone.value.replace(/\s+/g, '').length < 11) {
+    phone.classList.add('input--invalid');
+    errPhone.textContent = 'Введите корректный номер формата 9 999 999 99 99'
+  } else {
+    phone.classList.remove('input--invalid');
+    errPhone.textContent = ''
+  }
+
+  if (email.value.trim() === '') {
+    errEmail.textContent = 'Поле необходимо заполнить'
+    email.classList.add('input--invalid');
+  } else if(!validationEmail(email.value)) {
+    email.classList.add('input--invalid');
+    errEmail.textContent = 'Некорректный email'
+  } else {
+    email.classList.remove('input--invalid');
+    errEmail.textContent = ''
+  }
+
+})
+//Скрытие оплаты
+let checkbox = document.querySelector('.checkbox'),
+    paymentBlock = document.querySelector('.payment');
+
+
+checkbox.addEventListener('click', (e) => {
+  if(checkbox.checked) {
+    paymentBlock.classList.remove('visually-hidden');
+  } else {
+    paymentBlock.classList.add('visually-hidden');
+
+  }
+})
